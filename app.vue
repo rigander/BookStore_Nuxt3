3dd6da0b3ad8323d63d2a4ff9b89235cbeabe2a4
Andrey Komov <andrewkomov@gmail.com>
2023-11-20 14:31:42 +0200

    styles

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div>BookStore</div>
   </div>
 </template>
---
+++ nuxt.config.js
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+// @ts-ignore
+// @ts-ignore
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: [
+    "~/assets/style/scss/main.scss",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
