be1b0270b7c8c7ee780b83a945585e06a7237c0a
Andrey Komov <andrewkomov@gmail.com>
2023-11-23 01:22:14 +0200

    in progress

---
+++ components/mainpage/bestsellers/singleproduct.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <a href="">
+            <img src="~/assets/img/BestSellers/2.png" alt="image">
+        </a>
+        <p class="war-of-art">The war of Art</p>
+        <span>$60</span>
+    </div>
+</template>
---
+++ components/mainpage/pagination.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div id="page-number">
+        <div class="one"><a href="">1</a></div>
+        <div class="two"><a href="">2</a></div>
+        <div class="three"><a href="">3</a></div>
+        <div class="four"><a href="">4</a></div>
+        <div class="five"><a href="">5</a></div>
+        <div class="six"><a href="">6</a></div>
+    </div>
+</template>
