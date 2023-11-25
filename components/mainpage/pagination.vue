2b97d3df9e81b7417ce7e09aa7d29304217ef6f5
Andrey Komov <andrewkomov@gmail.com>
2023-11-26 01:31:44 +0200

    pagination 2

--- components/mainpage/pagination.vue
+++ components/mainpage/pagination.vue
@@ -1,15 +1,45 @@
-<script setup>
+<script setup lang="ts">
+import { ref } from "vue";
+const onClickHandler = (page: number) => {
+    console.log(page);
+};
 
+const currentPage = ref(1);
 </script>
 
-
 <template>
-    <div id="page-number">
-        <div class="one"><a href="">1</a></div>
-        <div class="two"><a href="">2</a></div>
-        <div class="three"><a href="">3</a></div>
-        <div class="four"><a href="">4</a></div>
-        <div class="five"><a href="">5</a></div>
-        <div class="six"><a href="">6</a></div>
-    </div>
+    <vue-awesome-paginate
+        :total-items="50"
+        :items-per-page="5"
+        :max-pages-shown="5"
+        v-model="currentPage"
+        :on-click="onClickHandler"
+    />
 </template>
+
+<style>
+.pagination-container {
+    display: flex;
+    column-gap: 10px;
+}
+.paginate-buttons {
+    height: 40px;
+    width: 40px;
+    border-radius: 20px;
+    cursor: pointer;
+    background-color: rgb(242, 242, 242);
+    border: 1px solid rgb(217, 217, 217);
+    color: black;
+}
+.paginate-buttons:hover {
+    background-color: #d8d8d8;
+}
+.active-page {
+    background-color: #3498db;
+    border: 1px solid #3498db;
+    color: white;
+}
+.active-page:hover {
+    background-color: #2988c8;
+}
+</style>
