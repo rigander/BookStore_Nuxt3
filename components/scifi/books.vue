1bc844dc35e92b2dcb6e5792d79f820558db9abd
Andrey Komov <andrewkomov@gmail.com>
2023-11-25 03:15:12 +0200

    sci fi books arrya

--- components/scifi/books.vue
+++ components/scifi/books.vue
@@ -1,37 +1,131 @@
-<script setup lang="ts">
-
+<script setup>
+let scifiBooks;
+scifiBooks = [
+    {
+        name: "The Hare With Amber Eyes",
+        image: 1,
+        price: 50,
+        discount: 1,
+    },{
+        name: "The war of Art",
+        image: 2,
+        price: 60,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 3,
+        price: 25,
+        discount: 1,
+    },{
+        name: "The Immortals of Meluha",
+        image: 4,
+        price: 80,
+        discount: 0,
+    },{
+        name: "Life is what you make it",
+        image: 5,
+        price: 200,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 6,
+        price: 200,
+        discount: 0,
+    },{
+        name: "The war of Art",
+        image: 7,
+        price: 150,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 8,
+        price: 100,
+        discount: 1,
+    },{
+        name: "The Immortals of Meluha",
+        image: 9,
+        price: 152,
+        discount: 0,
+    },{
+        name: "Life is what you make it",
+        image: 10,
+        price: 200,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 11,
+        price: 220,
+        discount: 0,
+    },{
+        name: "The war of Art",
+        image: 12,
+        price: 10,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 13,
+        price: 22,
+        discount: 0,
+    },{
+        name: "The Immortals of Meluha",
+        image: 14,
+        price: 300,
+        discount: 1,
+    },{
+        name: "Life is what you make it",
+        image: 15,
+        price: 205,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 11,
+        price: 220,
+        discount: 0,
+    },{
+        name: "The war of Art",
+        image: 12,
+        price: 10,
+        discount: 17,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 13,
+        price: 22,
+        discount: 0,
+    },{
+        name: "The Immortals of Meluha",
+        image: 14,
+        price: 300,
+        discount: 1,
+    },{
+        name: "Life is what you make it",
+        image: 2,
+        price: 205,
+        discount: 0,
+    },{
+        name: "The Hare With Amber Eyes",
+        image: 15,
+        price: 50,
+        discount: 1,
+    },
+];
 </script>
 
 
 <template>
     <div class="books__sci-fi">
-        <div><SharedDiscountLabel/><a href=""><img src="~/assets/img/BestSellers/1.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$50</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/2.png" alt="image"><p>The war of </p><span class="art">Art</span></a><span>$60</span></div>
-        <div><SharedDiscountLabel/><a href=""><img src="~/assets/img/BestSellers/3.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$25</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/4.png" alt="image"><p class="immortals">The Immortals of Meluha</p></a><span>$80</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/5.png" alt="image"><p>Life Is What You Make It</p></a><span>$200</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/6.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$200</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/7.png" alt="image"><p>The war of</p><span class="art">Art</span></a><span>$150</span></div>
-        <div><SharedDiscountLabel/><a href=""><img src="~/assets/img/BestSellers/8.png" alt="image">The Hare With Amber Eyes</a><span>$100</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/9.png" alt="image"><p class="immortals">The Immortals of Meluha</p></a><span>$152</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/10.png" alt="image"><p>Life Is What You Make It</p></a><span>$200</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/11.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$220</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/12.png" alt="image"><p>The war of </p><span class="art">Art</span></a><span>$10</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/13.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$22</span></div>
-        <div><SharedDiscountLabel/><a href=""><img src="~/assets/img/BestSellers/14.png" alt="image"><p class="immortals">The Immortals of Meluha</p></a><span>$300</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/15.png" alt="image"><p>Life Is What You Make It</p></a><span>$205</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/11.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$220</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/12.png" alt="image"><p>The war of </p><span class="art">Art</span></a><span>$10</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/13.png" alt="image"><p>The Hare With Amber Eyes</p></a><span>$22</span></div>
-        <div><SharedDiscountLabel/><a href=""><img src="~/assets/img/BestSellers/14.png" alt="image"><p class="immortals">The Immortals of Meluha</p></a><span>$300</span></div>
-        <div><a href=""><img src="~/assets/img/BestSellers/15.png" alt="image"><p>Life Is What You Make It</p></a><span>$205</span></div>
-        <ul id="page-number">
-            <li class="one"><a href="">1</a></li>
-            <li class="two"><a href="">2</a></li>
-            <li class="three"><a href="">3</a></li>
-            <li class="four"><a href="">4</a></li>
-            <li class="five"><a href="">5</a></li>
-            <li class="six"><a href="">6</a></li>
-        </ul>
+        <div v-for="book in scifiBooks">
+            <SharedDiscountLabel
+             v-if="book.discount === 1"
+            />
+            <a href="">
+                <img
+                    :src="`/img/BestSellers/${book.image}.png`"
+                    alt="image"
+                >
+                <p>{{ book.name }}</p>
+            </a>
+            <span>${{ book.price }}</span>
+        </div>
+        <ScifiPagination/>
     </div>
 </template>
---
+++ components/scifi/pagination/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <ul id="page-number">
+        <li class="one"><a href="">1</a></li>
+        <li class="two"><a href="">2</a></li>
+        <li class="three"><a href="">3</a></li>
+        <li class="four"><a href="">4</a></li>
+        <li class="five"><a href="">5</a></li>
+        <li class="six"><a href="">6</a></li>
+    </ul>
+</template>
