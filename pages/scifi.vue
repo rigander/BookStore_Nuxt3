bcf456e0938cccd7119c7f373c9ce76d98f1acfe
Andrey Komov <andrewkomov@gmail.com>
2023-12-06 19:34:52 +0200

    books styles

--- pages/scifi.vue
+++ pages/scifi.vue
@@ -6,8 +6,8 @@
 
 <template>
     <main id="main-content-wrapper-scifi">
-        <div class="books-li_container">
-            <section class="product-list">
+        <div class="books-li_container-scifi">
+            <section class="product-list-scifi">
                 <div class="title_sci-fi"><h4>Science Fiction</h4></div>
                 <ScifiBooks/>
             </section>
