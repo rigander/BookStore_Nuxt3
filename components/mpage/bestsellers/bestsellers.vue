1318cfb465b30f992c4ffc79e09d96e89bc67164
Andrey Komov <andrewkomov@gmail.com>
2023-12-07 01:46:28 +0200

    main page corrections styles

--- components/mpage/bestsellers/bestsellers.vue
+++ components/mpage/bestsellers/bestsellers.vue
@@ -5,97 +5,106 @@ BSbooks = [
         name: "The Hare With Amber Eyes",
         image: 1,
         price: 50,
-        discount: 1,
+        discount: true,
+        transfer: false
     },
     {
         name: "The war of Art",
         image: 2,
         price: 60,
-        discount: 0,
+        discount: false,
+        transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 3,
         price: 25,
-        discount: 1,
+        discount: true,
+        transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 4,
         price: 80,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "Life is what you make it",
         image: 5,
         price: 200,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 6,
         price: 200,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "The war of Art",
         image: 7,
         price: 150,
-        discount: 0,
+        discount: true,
+        transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 8,
         price: 100,
-        discount: 1,
+        discount: true,
+        transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 9,
         price: 152,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "Life is what you make it",
         image: 10,
         price: 200,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 11,
         price: 220,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "The war of Art",
         image: 12,
         price: 10,
-        discount: 0,
+        discount: false,
+        transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 13,
         price: 22,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 14,
         price: 300,
-        discount: 1,
+        discount: true,
+        transfer: false
     },
     {
         name: "Life is what you make it",
         image: 15,
         price: 205,
-        discount: 0,
-    },
-    {
-        name: "The Hare With Amber Eyes",
-        image: 11,
-        price: 220,
-        discount: 0,
+        discount: false,
+        transfer: false
     },
 ];
 </script>
@@ -105,15 +114,18 @@ BSbooks = [
     <div class="product-list__best-sellers">
         <div v-for="book in BSbooks">
             <SharedDiscountLabel
-            v-if="book.discount === 1"
+            v-if="book.discount"
             />
-            <a href=""><img
+            <a class="bs-anchor-book" href="">
+                <img
                 :src="`/img/BestSellers/${book.image}.png`"
                 alt="image"
             >
+                <p
+                :class="[book.transfer ? 'p-bests' : '']"
+                >{{ book.name }}</p>
+                <span>${{ book.price }}</span>
             </a>
-            <p>{{ book.name }}</p>
-            <span>${{ book.price }}</span>
         </div>
         <SharedPagination
         />
--- components/scifi/books.vue
+++ components/scifi/books.vue
@@ -5,147 +5,147 @@ scifiBooks = [
         name: "The Hare With Amber Eyes",
         image: 1,
         price: 50,
-        discount: 1,
+        discount: true,
         transfer: false
     },
     {
         name: "The war of Art",
         image: 2,
         price: 60,
-        discount: 0,
+        discount: false,
         transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 3,
         price: 25,
-        discount: 1,
+        discount: true,
         transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 4,
         price: 80,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "Life is what You Make it",
         image: 5,
         price: 200,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 6,
         price: 200,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The war of Art",
         image: 7,
         price: 150,
-        discount: 0,
+        discount: false,
         transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 8,
         price: 100,
-        discount: 1,
+        discount: true,
         transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 9,
         price: 152,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "Life is what You Make it",
         image: 10,
         price: 200,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 11,
         price: 220,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The war of Art",
         image: 12,
         price: 10,
-        discount: 0,
+        discount: false,
         transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 13,
         price: 22,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 14,
         price: 300,
-        discount: 1,
+        discount: true,
         transfer: false
     },
     {
         name: "Life is what You Make it",
         image: 15,
         price: 205,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 11,
         price: 220,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The war of Art",
         image: 12,
         price: 10,
-        discount: 17,
+        discount: true,
         transfer: true
     },
     {
         name: "The Hare With Amber Eyes",
         image: 13,
         price: 22,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Immortals of Meluha",
         image: 14,
         price: 300,
-        discount: 1,
+        discount: false,
         transfer: false
     },
     {
         name: "Life is what You Make it",
         image: 2,
         price: 205,
-        discount: 0,
+        discount: false,
         transfer: false
     },
     {
         name: "The Hare With Amber Eyes",
         image: 15,
         price: 50,
-        discount: 1,
+        discount: true,
         transfer: false
     },
 ];
@@ -156,7 +156,7 @@ scifiBooks = [
     <div class="books__sci-fi">
         <div v-for="book in scifiBooks">
             <SharedDiscountLabel
-             v-if="book.discount === 1"
+             v-if="book.discount"
             />
             <a href="">
                 <img class="book-img-scifi"
