c27b427fd2bc8567d2850def332c9fce7bd0cf08
Andrey Komov <andrewkomov@gmail.com>
2023-12-13 23:42:51 +0200

    double chevron decomposition and sass updates

---
+++ components/product/breadcrumps/breadcrumps.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const items = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "Discount and Clearance",
+        link: "/scifi"
+    },
+    {
+        name: "Sonatini Hippeastrum Alaska",
+        link: "/product"
+    },
+]
+</script>
+
+
+<template>
+    <div class="product-info__nav-route">
+        <ul class="bread-cramps">
+            <li
+                v-for="item in items"
+            >
+                <div
+                    v-if="item.name !== 'Home'"
+                >
+                    <ProductBreadcrumpsDchevron/></div>
+                <a :href="item.link">{{ item.name }}</a>
+            </li>
+        </ul>
+    </div>
+</template>
