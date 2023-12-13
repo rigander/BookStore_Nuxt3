ffd08cf58290c083ba1817ea1b02516fc8769b7c
Andrey Komov <andrewkomov@gmail.com>
2023-12-13 22:33:00 +0200

    tabs dynamic changes

--- components/mpage/prod-li/wrap.vue
+++ components/mpage/prod-li/wrap.vue
@@ -1,28 +1,32 @@
 <script setup lang="ts">
-const data = [
-    {
-        name: "New Arrivals",
-        table: "tab.NEW_ARRIVALS",
-        link: ""
-    },
-    {
-        name: "Used Books",
-        table: "tab.USED_BOOKS",
-        link: ""
-    },
-    {
-        name: "Special Offers",
-        table: "tab.SPECIAL_OFFERS",
-        link: ""
-    },
-]
-
 enum tab {
-    BEST_SELLERS = "b-sek",
+    BEST_SELLERS = "b-sel",
     NEW_ARRIVALS = "n-arr",
     USED_BOOKS = "u-books",
     SPECIAL_OFFERS = "s-offers",
 }
+const data = [
+    {
+        name: "Best Sellers",
+        table: tab.BEST_SELLERS,
+        link: ""
+    },
+    {
+        name: "New Arrivals",
+        table: tab.NEW_ARRIVALS,
+        link: ""
+    },
+    {
+        name: "Used Books",
+        table: tab.USED_BOOKS,
+        link: ""
+    },
+    {
+        name: "Special Offers",
+        table: tab.SPECIAL_OFFERS,
+        link: ""
+    },
+]
 interface OptionsState {
     genre: string;
 }
@@ -30,6 +34,11 @@ interface OptionsState {
 const opt = reactive<OptionsState>({
     genre: tab.BEST_SELLERS
 })
+
+const changeGenre = (event: Event, genre: string) => {
+    event.preventDefault();
+    opt.genre = genre;
+};
 </script>
 
 
@@ -40,23 +49,12 @@ const opt = reactive<OptionsState>({
                 <div class="product-list__nav">
                     <ul>
                         <li
-                            :class="opt.genre === tab.BEST_SELLERS && 'active'"
-                            @click="opt.genre = tab.BEST_SELLERS"
-                            class="option"
-                        >
-                            <div>
-                                <a href="">Best Sellers</a>
-                            </div>
-                        </li>
-                        <li
                             v-for="item in data"
                             :class="opt.genre === item.table && 'active'"
-                            @click="opt.genre = item.table"
+                            @click="changeGenre($event, item.table)"
                             class="option"
-                            >
-                            <div>
-                                <a href="">{{ item.name }}</a>
-                            </div>
+                        >
+                            <div><a href="">{{ item.name }}</a></div>
                         </li>
                     </ul>
                     <div class="big-box"></div>
