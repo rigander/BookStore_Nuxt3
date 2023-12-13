aca23d131bbe8cec66d40ad33939d714582581fb
Andrey Komov <andrewkomov@gmail.com>
2023-12-13 19:33:43 +0200

    tabs dynamic change css on-click

--- components/mpage/prod-li/wrap.vue
+++ components/mpage/prod-li/wrap.vue
@@ -1,6 +1,17 @@
-<script setup>
+<script setup lang="ts">
+enum tab {
+    BEST_SELLERS = "b-sek",
+    NEW_ARRIVALS = "n-arr",
+    USED_BOOKS = "u-books",
+    SPECIAL_OFFERS = "s-offers",
+}
+interface OptionsState {
+    genre: string;
+}
 
-
+const opt = reactive<OptionsState>({
+    genre: tab.SPECIAL_OFFERS
+})
 </script>
 
 
@@ -11,20 +22,28 @@
                 <div class="product-list__nav">
                     <ul>
                         <li
-                            class="option active"
+                            :class="opt.genre === tab.BEST_SELLERS && 'active'"
+                            @click="opt.genre = tab.BEST_SELLERS"
+                            class="option"
                             >
                             <div><a href="#">Best Sellers</a></div>
                         </li>
                         <li
+                            :class="opt.genre === tab.NEW_ARRIVALS && 'active'"
+                            @click="opt.genre = tab.NEW_ARRIVALS"
                             class="option"
                             >
                             <div ><a href="#">New Arrivals</a></div>
                         </li>
                         <li
+                            :class="opt.genre === tab.USED_BOOKS && 'active'"
+                            @click="opt.genre = tab.USED_BOOKS"
                             class="option"
                             >
                             <div ><a href="#">Used Books</a></div></li>
                         <li
+                            :class="opt.genre === tab.SPECIAL_OFFERS && 'active'"
+                            @click="opt.genre = tab.SPECIAL_OFFERS"
                             class="option"
                             >
                             <div ><a href="#">Special Offers</a></div>
