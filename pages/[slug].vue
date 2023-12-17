5c4595becde0915a20bc82567cf769aa98521766
Andrey Komov <andrewkomov@gmail.com>
2023-12-17 22:22:24 +0200

    get stack...

---
+++ pages/[slug].vue
@@ -0,0 +1,16 @@
+<script setup>
+const booksStore = useCategoryStore();
+const data = booksStore.books.value;
+</script>
+
+
+<template>
+    <div>
+        <h1>Category: {{ booksStore.selectedSlug }}</h1>
+        <ul>
+<!--            <li v-for="item in data">-->
+<!--                {{ item.name }}-->
+<!--            </li>-->
+        </ul>
+    </div>
+</template>
--- stores/CategoryStore.js
+++ stores/CategoryStore.js
@@ -1,17 +1,34 @@
 import { defineStore } from 'pinia'
 
-export const useCategoryStore = defineStore({
-    id: 'category',
-    state: () => ({
-        selectedSlug: null,
-        books: [],
-    }),
-    actions: {
-        setSelectedSlug(slug) {
-            this.selectedSlug = slug
-        },
-        setBooks(books) {
-            this.books = books
-        },
-    },
-})
+export const useCategoryStore = defineStore(
+    'category',
+    () => {
+        const selectedSlug = ref(null);
+        let books = ref([]);
+
+        const setBooks = (newBooks) => {
+            books.value = newBooks;
+            console.log('books:', books);
+            console.log('inside', books.value);
+        };
+
+        const loadBooks = async () => {
+            if (!selectedSlug.value) return;
+            const uri = `http://api.book-store.loc/api/category/${selectedSlug.value}/books?page=1`;
+            const { data: booksResponse } = await useFetch(uri);
+            setBooks(booksResponse);
+        };
+
+        const setSelectedSlug = (slug) => {
+            selectedSlug.value = slug;
+            loadBooks();
+        };
+
+        return {
+            selectedSlug,
+            setSelectedSlug,
+            loadBooks,
+            books
+        };
+    }
+);
