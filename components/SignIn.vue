d172b7c5d7fd13f5e1d5ac4c896b4bb4304dfcc8
Andrey Komov <andrewkomov@gmail.com>
2023-12-23 03:40:13 +0200

    sign in modal value

--- components/SignIn.vue
+++ components/SignIn.vue
@@ -22,13 +22,45 @@ const hideDialog = () => {
         <div
             @click.stop
             class="dialog__content">
-            <slot></slot>
+            <form action="">
+                <h1>Sign In</h1>
+                <div class="sign-in__container">
+                    <div class="sign-in__username">
+                        <span class="invalidUserName validUserName"></span>
+                        <label for="login">Username</label>
+                        <input class="all-inputs username" minlength="2"
+                               placeholder=""
+                               id="username" name="username"
+                               type="text">
+                    </div>
+                    <NuxtLink>
+                        <div>Forgot password?</div>
+                    </NuxtLink>
+                    <div class="sign-in__password">
+                        <span class="invalidPassword validPassword"></span>
+                        <label for="login">Password</label>
+                        <input class="all-inputs password" minlength="2"
+                               placeholder=""
+                               id="password" name="password"
+                               type="text">
+                    </div>
+                    <div class="sign-in-button">
+                        <input name="signInButton" id="sign-in-but" value="Sing In" type="submit">
+                    </div>
+                    <NuxtLink>
+                        <div>Create Account</div>
+                    </NuxtLink>
+                </div>
+            </form>
         </div>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
+form{
+    padding: 20px;
+}
 .dialog{
   top: 0;
   bottom: 0;
@@ -43,7 +75,7 @@ const hideDialog = () => {
     margin: auto;
     background: white;
     border-radius: 10px;
-    min-height: 500px;
-    min-width: 350px;
+    height: 500px;
+    width: 350px;
 }
 </style>
