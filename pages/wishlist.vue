155e75cc74bd3d007838c5d9e44733f00cf4280e
Andrey Komov <andrewkomov@gmail.com>
2024-01-12 01:52:17 +0200

    wishlist page

---
+++ pages/wishlist.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="wishlist-wrapper">
+        <div class="wishlist">
+            <h1>Your Wishlist</h1>
+            <NuxtLink class="wishlist_book">
+                <img id="wishlist_book-image" alt="book-img" src="">
+                <span class="wishlist_book-title">title</span>
+                <span class="wishlist_book-cost">cost$</span>
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.wishlist{
+  width: 800px;
+  background-color: #f4f4f4;
+}
+</style>
