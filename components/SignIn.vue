ade4e9ca96ae9d23eddec3f8a2058ad633139b3e
Andrey Komov <andrewkomov@gmail.com>
2024-01-22 01:48:48 +0200

    updates

--- components/SignIn.vue
+++ components/SignIn.vue
@@ -45,29 +45,6 @@ const submitSignInform = async () => {
         console.error('An unexpected error occurred:', error);
     }
 }
-const signin = async () => {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const {data: {access_token}} = await useFetch(
-        `${apiBaseUrl}/auth/login`,
-        {
-            method: 'post',
-            body: {
-                username,
-                password
-            }
-        });
-
-    const { data: userData, error } = await useFetch(
-        'http://api.book-store.loc/api/user',
-        {
-            method: 'get',
-            headers: {
-                Authorization: `Bearer ${access_token}`
-            }
-        }
-    );
-}
 </script>
 
 
