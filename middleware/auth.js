c7be7c111533d78f4ffbd07d4c3a55ddc60123f0
Andrey Komov <andrewkomov@gmail.com>
2024-01-24 02:24:52 +0200

    auth update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const profileRoute = '/profile';
+    const checkoutRoute = '/checkout';
     const {state} = useProfileStore();
     const modalStore = useModalStore();
     const token = state.token;
 
-    if (to.fullPath === profileRoute) {
+    if (to.fullPath === profileRoute || to.fullPath === checkoutRoute) {
         if (!token) {
             if (to.path !== '/'){
                 modalStore.showModal();
